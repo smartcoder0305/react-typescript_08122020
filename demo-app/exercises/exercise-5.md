835d580366b20b4f64bedd033d8f43fca3f3cc81
smartcoder0305 <stunningcoder7@gmail.com>
2020-08-12 19:09:17 -0400

    add exercise 5

---
+++ demo-app/exercises/exercise-5.md
@@ -0,0 +1,7 @@
+# Exercise 5
+
+1. Create a car form. Add fields for collecting the car make, model, year, color and price. Do not create an input field for the id.
+
+2. On each keystroke in any of the input fields, output the state data object to the console.
+
+3. Ensure it works.
--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,22 +1,51 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 
 import { Color } from '../models/Color';
 
 type ColorToolProps = {
-  colors: Color[],
+  colors: Readonly<Color[]>,
 };
 
 export const ColorTool: FC<ColorToolProps> = (props) => {
 
+  const [
+    colorForm /* state data */,
+    setColorForm /* function to update the state data, and triggering the re-render */
+  ] = useState({
+    name: '',
+    hexcode: '',
+  } /* initializing the state to this value */);
+
+  const change = (e: ChangeEvent<HTMLInputElement>) => {
+    setColorForm({
+      ...colorForm, // copy the current color form properties on to the new state object
+      [ e.target.name ]: e.target.value, // set the name property of the new state object to the data collected from the input field
+    });
+  };
+
+  console.log(colorForm);
+
   return (
     <>
       <header>
-        <h1>Color Tool</h1>
+        <h1 className="page-header">Color Tool</h1>
       </header>
       <ul>
         {props.colors.map(color =>
           <li key={color.id}>{color.name}</li>)}
       </ul>
+      <form>
+        <div>
+          {/* React.createElement('label', { htmlFor: 'color-name-input}, 'Color Name: ') */}
+          <label htmlFor="color-name-input">Color Name:</label>
+          <input type="text" id="color-name-input" name="name" value={colorForm.name} onChange={change} />
+        </div>
+        <div>
+          {/* React.createElement('label', { htmlFor: 'color-name-input}, 'Color Name: ') */}
+          <label htmlFor="color-hexcode-input">Color Hexcode:</label>
+          <input type="text" id="color-hexcode-input" name="hexcode" value={colorForm.hexcode} onChange={change} />
+        </div>
+      </form>
     </>
   );
 
